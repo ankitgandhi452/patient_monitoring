e21049c356d1df4c795136707d6576b594f304a2
Ankit Gandhi <ankit.gandhi@quodeck.com>
2018-04-06 11:25:38 +0530

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     mount RailsAdmin::Engine => '/support_panel', as: 'rails_admin'
   end
   mount_devise_token_auth_for 'User', at: 'auth'
+  root to: redirect("/auth/support_panel")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
